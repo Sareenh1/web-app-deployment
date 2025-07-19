186f136dc75b987ad19aa30db979ec7c57bd7e01
Ubuntu <ubuntu@ip-172-31-37-223.ap-south-1.compute.internal>
2025-07-19 08:25:00 +0000

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "web_server" {
+  count         = 10
+  ami           = "ami-0f918f7e67a3323f0"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "web-server-${count.index + 1}"
+  }
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install -y docker.io
+              sudo systemctl enable --now docker
+              EOF
+}
+
+output "instance_ips" {
+  value = aws_instance.web_server[*].public_ip
+}
